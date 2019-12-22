d7e7c2d55fbc1915331a0009ee8aa7d1ab21226b
menKan <xtz17515@163.com>
2019-12-22 21:19:40 +0800

    change ejs template; version 0.0.2

--- src/_demos/app.js
+++ src/_demos/app.js
@@ -8,23 +8,51 @@ const fs = require('fs');
 const ejs = require('ejs');
 
 const data = {
-  title: 'EveryDay Tips',
-  auther: 'menkan_mark',
-  banner_im: '',
-  description: '热爱，就是一种天赋。你不一定最优秀，但你有一股冲劲儿，哪怕自己干得不行，也不想放弃，这就是天赋。',
-  tag: '摄影',
-  weatherArr: [
+  title: 'everyday tips',
+  auther: 'everyday tips',
+  "banner":"http://image.wufazhuce.com/Foek-9Pwyj844tDCLT304YTrFz9L",
+  "tag":"摄影","description":"我只是无法明白，到底是爱艰难些，还是承受爱艰难些。",
+  "middleInfo":{
+    "tips": "天冷了，该加衣服了！",
+    "temperature":"9",
+    "_topImage":"https://h5tq.moji.com/tianqi/assets/images/weather/w0.png",
+    "_topTxt":"晴"
+  },
+  "weatherArr":[
     {
-      time: '',
-      src: '',
-      state: '',
-      desc: '',
-      _clas: 'level_6',
+      "time":"今天",
+      "src":"https://h5tq.moji.com/tianqi/assets/images/weather/w0.png",
+      "state":"晴",
+      "scope":"4° / 11°",
+      "directionOfTheWind":"西北风",
+      "level":"3级",
+      "pollution":"115 轻度污染",
+      "class":"level_3"
+    },
+    {
+      "time":"明天",
+      "src":"https://h5tq.moji.com/tianqi/assets/images/weather/w0.png",
+      "state":"晴",
+      "scope":"6° / 12°",
+      "directionOfTheWind":"北风",
+      "level":"3级",
+      "pollution":"84 良",
+      "class":"level_2"
+    },
+    {
+      "time":"后天",
+      "src":"https://h5tq.moji.com/tianqi/assets/images/weather/w1.png",
+      "state":"多云",
+      "scope":"8° / 13°",
+      "directionOfTheWind":"北风",
+      "level":"3-4级",
+      "pollution":"63 良",
+      "class":"level_2"
     }
   ]
 };
 
-let indexPath = path.join(__dirname, 'view', 'index.ejs');
+let indexPath = path.join(__dirname, '..', '..', 'view', 'index.ejs');
 
 // demo
 // const template = ejs.compile('<h3>hello <%= title %> </h3>');
